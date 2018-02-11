6de31ff5954db7530f9e09aebfcc63e26f18f6c4
rpalermo <rpalermo@mit.edu>
2018-02-11 17:41:33 -0500

    flexible n communities
    n of communities are flexible in the main code. Just needs to be changed in inputs now.

--- GeoBarrierEcon/GeoBarrier_Inputs.m
+++ GeoBarrierEcon/GeoBarrier_Inputs.m
@@ -41,17 +41,19 @@ astfac = .5; % fractional diffusivity (i.e. angular wave distribution reduces di
 K2=0.34/2*astfac; %m^(3/5)s^(-6/5)
 Ka=K2*Ho^(12/5)*To^(1/5) * 365*86400; %convert to m^3/year
 
-%%%% Community location inputs
-% jjcom = zeros(size(Yi));
-jjcom1 = 21:25;             % 1 represents community 1
-jjcom2 = 56:60;             % 2 represents community 2
-com1w0 = 49;                % 1 initial width *NRM paper (49 meters)
-com2w0 = 49;                % 2 initial width *NRM paper
+%%%% Community variables to explicitly define for each
+ncom = 2;                      % number of communities
+com(1).jj = 21:25;             % 1 represents community 1
+com(2).jj = 56:60;             % 2 represents community 2
+com(1).w0 = 49;                % 1 initial width *NRM paper (49 meters)
+com(2).w0 = 49;                % 2 initial width *NRM paper
 propertysize = 50;          % width of properties (cross-shore)
 nproperties = 2;              % number of properties per dy
 dem = 8000;                  % estimated cost of demolishing a home ~1500-2000 sq ft
-L1 = (jjcom1(end)-jjcom1(1))*dy;    %length of comm 1
-L2 = (jjcom2(end)-jjcom2(1))*dy;    %length of comm 2
+com(1).L = (com(1).jj(end)-com(1).jj(1))*dy;    %length of comm 1
+com(2).L = (com(2).jj(end)-com(2).jj(1))*dy;    %length of comm 2
+com(1).color = 'c';
+com(2).color = 'm';
 
 %%%% Community and Cost Benefit Analysis Controls--- (Slott 2008)
 f = 1e6;    % fixed cost of nourishment
@@ -59,17 +61,17 @@ f = 1e6;    % fixed cost of nourishment
 %this is where I will add something having to do with multiple existing
 %properties and their value relative to distance from oceanfront and width
 %of the beach
-P1 = 1.5e6; %NRM 2013 (1.5e6)
-P2 = 1.5e6; %same as P1--- needs to be changed to coastal commercial MA value?
+com(1).P = 1.5e6; %NRM 2013 (1.5e6)
+com(2).P = 1.5e6; %same as P1--- needs to be changed to coastal commercial MA value?
 ir = 0.07; % 7% discount rate
 nyears = 5; % nyears of project (how long they look into the future)
 n = 25; % 25 years (NRM 2013)
-alpha1 = P1*ir*((1+ir)^n)./(((1+ir)^n)-1);     % annual value of community 1
-alpha2 = P2*ir*((1+ir)^n)./(((1+ir)^n)-1);     % annual value of community 2
+com(1).alpha = com(1).P*ir*((1+ir)^n)./(((1+ir)^n)-1);     % annual value of community 1
+com(2).alpha = com(2).P*ir*((1+ir)^n)./(((1+ir)^n)-1);     % annual value of community 2
 c = 5;              % unit cost of sand /volume
 b = 0.2632;          % exponent from Pompe & Rinehard 1995-- first row of houses = 0.2632; OR Landry 2007 (whole community)= 0.085, and NRM 2013 =0.157 (reasonable for coastal homes in NE)
-Wn1 = 20; %width to add in nourishment
-Wn2 = 20; %width to add in nourishment
+com(1).Wn = 20; %width to add in nourishment
+com(2).Wn = 20; %width to add in nourishment
 
 
 % Plot Control
@@ -96,20 +98,25 @@ Y=0:dy:Yn*dy;    % real y array
 ys=length(Y);    % alongshore spots
 Yi = 1:ys;       % Y i's
 
-%% after run 1 comment out this section
+%%% zero variables for each community
+for c = 1:ncom
+    com(c).Xvnn = 0;
+    com(c).Vnn = 0;
+    com(c).tnourished = zeros(ts,length(runs));
+    com(c).tmanret = zeros(ts,length(runs));
+    com(c).nnourished = zeros(1,length(runs));
+    com(c).nmanret = nan(1,length(runs));
+    com(c).inourished = zeros(ts,length(runs));
+    com(c).imanret = zeros(ts,length(runs));
+    com(c).TBtwN = nan(ts,length(runs));
+    com(c).TBtwMR = nan(ts,length(runs));
+    com(c).NB = zeros(ts,length(runs));
+    com(c).NBmr = zeros(ts,length(runs));
+    com(c).action = zeros(ts,length(runs));
 
-tnourished1 = zeros(ts,length(runs));
-tnourished2 = zeros(ts,length(runs));
-tmanret1 = zeros(ts,length(runs));
-tmanret2 = zeros(ts,length(runs));
-inourished1 = zeros(ts,length(runs));
-inourished2 = zeros(ts,length(runs));
-imanret1 = nan(ts,length(runs));
-imanret2 = nan(ts,length(runs));
-TBtwN1 = nan(ts,length(runs));
-TBtwN2 = nan(ts,length(runs));
-TBtwMR1 = nan(ts,length(runs));
-TBtwMR2 = nan(ts,length(runs));
+end
+
+
 %%%% Cost Benefit Analysis stuff
 Beta1 = zeros(ts,length(runs));
 Beta2 = zeros(ts,length(runs));
